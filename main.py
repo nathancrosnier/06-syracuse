39064a8973ee9fbc69494fe36f29c0897eaf8e5a
nathancrosnier <nathan.crosnier@edu.esiee.fr>
2024-11-14 08:52:50 +0000

    Ex06

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+"""
+Ex06
+"""
 #### Fonctions secondaires
 
 
@@ -6,6 +9,9 @@ from plotly.graph_objects import Scatter, Figure
 
 ### NE PAS MODIFIER ###
 def syr_plot(lsyr):
+    """
+    Graphe
+    """
     title = "Syracuse" + " (n = " + str(lsyr[0]) + " )"
     fig = Figure({  'layout':   { 'title': {'text': title},
                                 'xaxis': {'title': {'text':"x"}},
@@ -14,12 +20,11 @@ def syr_plot(lsyr):
                 }
     )
 
-    x = [ i for i in range(len(lsyr)) ]
+    x = list(range(len(lsyr)))
     t = Scatter(x=x, y=lsyr, mode="lines+markers", marker_color = "blue")
     fig.add_trace(t)
     fig.show()
     # fig.write_html('fig.html', include_plotlyjs='cdn')
-    return None
 #######################
 
 def syracuse_l(n):
@@ -31,9 +36,14 @@ def syracuse_l(n):
     Returns:
         list: la suite de Syracuse de source n
     """
-
-    # votre code ici 
-    l = [ ]
+    l = []
+    while n != 1:
+        l.append(n)
+        if n%2 == 0:
+            n/=2
+        else:
+            n = 3*n + 1
+    l.append(1)
     return l
 
 def temps_de_vol(l):
@@ -45,11 +55,7 @@ def temps_de_vol(l):
     Returns:
         int: le temps de vol
     """
-    
-    # votre code ici
-
-    n = 0
-    return n
+    return len(l)-1
 
 def temps_de_vol_en_altitude(l):
     """Retourne le temps de vol en altitude d'une suite de Syracuse
@@ -60,11 +66,7 @@ def temps_de_vol_en_altitude(l):
     Returns:
         int: le temps de vol en altitude
     """
-
-    # votre code ici
-
-    n = 0
-    return n
+    return next(i for i, x in enumerate(l) if x < l[0])-1
 
 
 def altitude_maximale(l):
@@ -76,18 +78,16 @@ def altitude_maximale(l):
     Returns:
         int: l'altitude maximale
     """
-    
-    # votre code ici
-    
-    n = 0
-    return n
+    return max(l)
 
 
 #### Fonction principale
 
 
 def main():
-
+    """
+    main
+    """
     # vos appels à la fonction secondaire ici
     lsyr = syracuse_l(15)
     syr_plot(lsyr)
